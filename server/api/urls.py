823d7ff28f606d08dd674a88ff1dfd9fb496b906
yoojsung <jaesung.john.yoo@gmail.com>
2024-06-20 19:43:00 -0700

    export to google sheets

--- server/api/urls.py
+++ server/api/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import ItemListAPIView, UploadAPIView, ScanAPIView
+from .views import ItemListAPIView, UploadAPIView, ExportGsAPIView
 
 urlpatterns = [
     path('items/', ItemListAPIView.as_view(), name='item-list'),
     path('upload/', UploadAPIView.as_view()),
-    path('scan/', ScanAPIView.as_view())
+    path('scan/', ScanAPIView.as_view()),
+    path('export/', ExportGsAPIView.as_view())
 ]
--- server/api/views.py
+++ server/api/views.py
@@ -7,6 +7,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 import pandas as pd
+import gspread
+from google.oauth2.service_account import Credentials
 
 
 class ItemListAPIView(APIView):
@@ -58,3 +60,32 @@ class ScanAPIView(APIView):
             print(row)
 
         return Response({"message": "good"}, status=status.HTTP_200_OK)
+
+
+class ExportGsAPIView(APIView):
+    SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+    SPREADSHEET_ID = '1o9OqsjPJZz4UI9z77e9bd6W-03cLiyN_pUVxyogZZFo'
+    TOKEN_PATH = r'C:\Users\jaesu\Documents\KEPC_Tech\JOSEPH\server\api\google-sheet-api-token.json'
+
+    def export_data_to_sheets(self):
+
+        with open(self.TOKEN_PATH, 'r') as file:
+            service_account_info = json.load(file)
+
+        creds = Credentials.from_service_account_info(
+            service_account_info, scopes=self.SCOPES)
+        client = gspread.authorize(creds)
+
+        sheet = client.open_by_key(self.SPREADSHEET_ID)
+
+        fields = [field.name for field in Item._meta.get_fields()]
+        items = Item.objects.all()
+        values_list = [
+            fields] + [[str(value) for value in item.values()] for item in items.values()]
+        print(values_list)
+        sheet.sheet1.clear()
+        sheet.sheet1.update(values_list)
+
+    def get(self, request):
+        self.export_data_to_sheets()
+        return Response({"message": "Data exported successfully."})
