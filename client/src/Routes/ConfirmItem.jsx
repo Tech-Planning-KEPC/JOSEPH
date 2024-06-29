b174b7e0f8b1b2b3c3b909b2cdfee0f8693f8f43
Yongjun-Kim-YJ <yongjun.j.kim@gmail.com>
2024-06-29 15:18:11 -0700

    committee dropdown select

--- client/src/Routes/ConfirmItem.jsx
+++ client/src/Routes/ConfirmItem.jsx
@@ -1,8 +1,9 @@
 import { useLocation, useNavigate, useParams } from "react-router-dom";
 import { styled } from "styled-components";
 import { useForm } from "react-hook-form";
-import { useState } from "react";
+import { useState, useRef} from "react";
 import { SERVER_URL } from "../api";
+import useDetectClose from "../Components/useDetectClose";
 
 const Container = styled.div`
   display: grid;
@@ -47,6 +48,33 @@ const PageContainer = styled.div`
   align-items: center;
 `;
 
+const StyledList = styled.ul`
+    position: absolute;
+    list-style-type: none;
+    padding: 5px;
+    margin: 1px;
+    background-color: white;
+    width: 40%;
+    right: 0;
+    border: 1px solid #ccc;
+    border-radius: 3px;
+    font: inherit;
+    font-size: 14px;
+    
+    .list-item {
+      margin: 0px 0;
+      height: 27px;
+      display: flex; /* 항목을 flex로 설정하여 */
+      align-items: center; /* 수직 가운데 정렬을 적용합니다 */
+      cursor: pointer;
+      transition: background-color 0.3s ease;
+    }
+
+    .list-item:hover {
+      background-color: #f0f0f0;
+    }
+  `;
+
 const excelSerialDateToJSDate = (serial) => {
   const utc_days = Math.floor(serial - 25569);
   const date_info = new Date(utc_days * 86400 * 1000);
@@ -65,7 +93,18 @@ export default function ConfirmItem() {
   const { register, handleSubmit, resetField } = useForm();
   const [modifiedData, setModifiedData] = useState(data);
 
+  const handleCommitteeSelect = (value) => {
+    setCommitteeIdentify(value);
+    setModifiedData((prevData) => {
+      const newData = [...prevData];
+      newData[page - 1].committee = value;
+      return newData;
+    });
+    setIsOpen(false);
+  };
+
   const handlePrevPage = () => {
+    setCommitteeIdentify(modifiedData[page-2].committee);
     resetFields();
     navigate(`/upload/${page - 1}`, { state: { modifiedData, totalPage } });
   };
@@ -73,6 +112,7 @@ export default function ConfirmItem() {
   const handleNextPage = () => {
     resetFields();
     navigate(`/upload/${page + 1}`, { state: { modifiedData, totalPage } });
+    setCommitteeIdentify(modifiedData[page].committee);
   };
 
   const onSubmit = (values) => {
@@ -116,6 +156,28 @@ export default function ConfirmItem() {
 
     console.log(data, res.status);
   };
+  
+  const dropDownRef = useRef();
+  const [CommitteeIdentify, setCommitteeIdentify] = useState(modifiedData[page - 1].committee);
+  const CommitteeList = [
+    '예배위원회',
+    '찬양위원회',
+    '신앙위원회',
+    '돌봄위원회',
+    '제1교육위원회',
+    '제2교육위원회',
+    '선교위원회',
+    '지역봉사위원회',
+    '전도위원회',
+    '새가족위원회',
+    '관리위원회',
+    '친교위원회',
+    '에녹위원회',
+    '비전청년위원회',
+    '재정위원회',
+    '가정교회 사역원']
+
+  const [isOpen, setIsOpen] = useDetectClose(dropDownRef, false);
 
   console.log(modifiedData[page - 1]);
   return (
@@ -140,14 +202,52 @@ export default function ConfirmItem() {
             {...register("name")}
           />
         </InputContainer>
+        
+        <div ref={dropDownRef} style={{ position: 'relative'}}>
         <InputContainer>
+        
           <Label>위원회</Label>
+          <div style={{  width: '101.18%' }}>
           <Input
             key={`committee-${page}`}
-            value={modifiedData[page - 1].committee}
+            value={CommitteeIdentify}
             {...register("committee")}
+            onClick={() => setIsOpen(!isOpen)}
+            type='button'
+            style={{ backgroundColor: 'white', textAlign: 'left' }}
           />
+          <span
+            style={{
+            position: 'absolute',
+            right: '8px',
+            top: '5%',
+            transform: 'translateY(-0%)',
+            pointerEvents: 'none', // 텍스트에 대한 클릭 이벤트를 비활성화하여 입력 필드의 클릭 이벤트가 작동하도록 함
+            }}
+          >
+          ⌄
+          </span>
+          </div> 
         </InputContainer>
+        {isOpen && 
+          <StyledList>
+          {CommitteeList.map((value, index) => (
+            <li
+              key={index}
+              className="list-item"
+              onClick={() => {
+                setCommitteeIdentify(value);
+                setIsOpen(false);
+                handleCommitteeSelect(value);
+              }}
+            >
+              {value}
+            </li>
+          ))}
+        </StyledList>
+        }
+        </div>
+        
         <InputContainer>
           <Label>부서</Label>
           <Input
